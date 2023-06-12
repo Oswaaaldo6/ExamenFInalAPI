1c6c4c6406d2418ceccbbd55cde8c9e149c739b9
Oswaaaldo6 <oswaaaldo6@gmail.com>
2023-06-12 01:14:44 -0600

    Descripción de los cambios

--- src/main/java/org/uv/examenfinal/Examen1OswaldoCoronaPerez.java
+++ src/main/java/org/uv/examenfinal/Examen1OswaldoCoronaPerez.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamenIraisAguirreValente {
+public class Examen1OswaldoCoronaPerez {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExamenIraisAguirreValente.class, args);
+		SpringApplication.run(Examen1OswaldoCoronaPerez.class, args);
 	}
 
 }
